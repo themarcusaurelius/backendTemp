ca47921e2e2dae9e1924d7161f6fe77de5873f82
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-17 12:50:35 -0500

    Changed Greeting

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 //Route Handler
 app.get('/', (req,res) => {
-    res.send({ hi: 'there' })
+    res.send({ hi: 'bye' })
 });
 //Dynamically figures out which port to listen to. If one hasn't been defined then by default use 5000
-const port = process.env.PORT || 5000
-//Instructs Express to tell Node to listen to port 5000
-app.listen(port);
+const PORT = process.env.PORT || 5000
+//Instructs Express to tell Node to listen to port
+app.listen(PORT);
 
 
