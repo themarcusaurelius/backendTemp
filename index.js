9d342301a93c6feb388a209f5254558195298eea
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-17 13:36:16 -0500

    Passport.js Set Up

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 //Express App to register the route handler with.
 const app = express();
-//Route Handler
-app.get('/', (req,res) => {
-    res.send({ hi: 'bye' })
-});
+//Route Handle - creates and registers a new instance of google strategy
+passport.use(new GoogleStrategy());
 //Dynamically figures out which port to listen to. If one hasn't been defined then by default use 5000
 const PORT = process.env.PORT || 5000
-//Instructs Express to tell Node to listen to port
+//Instructs Express to tell Node to listen to PORT
 app.listen(PORT);
 
 
