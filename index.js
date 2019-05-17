e28ebe2b7ed95d8325d4517a45808c5d4b0e4693
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-17 12:18:44 -0500

    Deployment Checklist: Dynamic Port Binding, Specify Node Enviroment and Start Script

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const app = express();
 app.get('/', (req,res) => {
     res.send({ hi: 'there' })
 });
+//Dynamically figures out which port to listen to. If one hasn't been defined then by default use 5000
+const PORT = process.env.PORT || 5000
 //Instructs Express to tell Node to listen to port 5000
 app.listen(5000);
 
