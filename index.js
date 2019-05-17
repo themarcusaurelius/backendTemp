4f5a9adfb72d36bc613a5c5a7354d887dc7832a2
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-17 12:44:17 -0500

    Debugging heroku

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ app.get('/', (req,res) => {
     res.send({ hi: 'there' })
 });
 //Dynamically figures out which port to listen to. If one hasn't been defined then by default use 5000
-const PORT = process.env.PORT || 5000
+const port = process.env.PORT || 5000
 //Instructs Express to tell Node to listen to port 5000
 app.listen(5000);
 
