8baaf4d1dfe4c04cf1273a8e67f5ab83ec3aad75
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-18 11:17:36 -0500

    Logging Out Users

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 const passport = require('passport');
 
 module.exports = app => {
-   //Route handler to handle passport authentication flow
+    //Route handler to handle passport authentication flow
     app.get(
         '/auth/google', 
         passport.authenticate('google', {
@@ -10,5 +10,17 @@ module.exports = app => {
     );
 
     //Route handler to handle if user visits /auth/google/callback
-    app.get('/auth/google/callback', passport.authenticate('google')) 
+    app.get('/auth/google/callback', passport.authenticate('google'))
+
+    //Route handler for logging out
+    app.get('/api/logout', (req,res) => {
+        req.logout();
+        //Aknowledges signout
+        res.send(req.user);
+    });
+    
+    //GET Request route handler to get access to current user logging in.
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user)
+    });
 }
