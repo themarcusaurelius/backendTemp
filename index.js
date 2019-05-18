81acbd8e342e762cbee9c4b47cbe27b3e0d632c2
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-18 11:01:24 -0500

    Cookies Enabled

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose')
+//Gives access to Cookies
+const cookieSession = require('cookie-session');
+//Tells Passport.js to make use of cookies
+const passport = require('passport');
 const keys = require('./config/keys');
 //Must be in this order to register the Schema for model 'users'
 require('./models/user');
@@ -10,6 +14,20 @@ mongoose.connect(keys.mongoURI, {useNewUrlParser: true});
 
 //Express App used to register the route handler with.
 const app = express();
+
+//Creates cookie session
+app.use(
+  cookieSession({
+      //How long cookie can exist in the browser
+      maxAge: 30 * 24 * 60 * 60 * 1000,
+      //Key used to encrypt cookie
+      keys: [keys.cookieKey]
+  })  
+);
+
+//Tells Passport to make use of cookies to handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
 
 //Returns a function and immedietley invokes app
 require('./routes/authRoutes')(app)
