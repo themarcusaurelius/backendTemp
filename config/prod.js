89206881c956eeea709dac56e12f362c7a0786e1
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-18 12:45:17 -0500

    Heroku Env Variables

--- config/prod.js
+++ config/prod.js
@@ -6,6 +6,3 @@ module.exports = {
     cookieKey: process.env.COOKIE_KEY,
 };
 
-//979998376622-gmhbmdmi3amkk2eu8qjrgb4a0jvher2f.apps.googleusercontent.com
-//anYdptQitKoKWixUlmRqLsra
-//mongodb+srv://myuser:04301987Ma!@cluster0-hpefh.mongodb.net/test?retryWrites=true
