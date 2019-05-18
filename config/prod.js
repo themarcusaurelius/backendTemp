dfcbc9dca2bdb7bd1b1773b1dbe9264e63a7c711
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-18 12:38:09 -0500

    Version Control Scheme

---
+++ config/prod.js
@@ -0,0 +1,11 @@
+// prod.js - production keys here
+module.exports = {
+    googleClientID: process.env.GOOGLE_CLIENT_ID,
+    googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    mongoURI: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+};
+
+//979998376622-gmhbmdmi3amkk2eu8qjrgb4a0jvher2f.apps.googleusercontent.com
+//anYdptQitKoKWixUlmRqLsra
+//mongodb+srv://myuser:04301987Ma!@cluster0-hpefh.mongodb.net/test?retryWrites=true
