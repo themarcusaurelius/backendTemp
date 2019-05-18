f962910e043dde470d5632a800469c23051015b6
Marcus Aurelius <markmayfield16@gmail.com>
2019-05-18 12:57:02 -0500

    Fixing Heroku Proxy Issues

--- services/passport.js
+++ services/passport.js
@@ -24,7 +24,8 @@ passport.use(
         {
             clientID: keys.googleClientID,
             clientSecret: keys.googleClientSecret,
-            callbackURL: '/auth/google/callback'
+            callbackURL: '/auth/google/callback', 
+            proxy: true
         }, 
         (accessToken, refreshToken, profile, done) => {
             //Atempt to find users already in database to not add again
